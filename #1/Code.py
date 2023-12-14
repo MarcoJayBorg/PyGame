9a6f020aa5a6c6135b0b7ae517714be5c48ab925
Marco Jay Borg <152505217+MarcoJayBorg@users.noreply.github.com>
2023-12-14 17:01:37 +0300

    Update Code.py

--- #1/Code.py
+++ #1/Code.py
@@ -1,1 +1,12 @@
+import pygame, sys
 
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+screen = pygame.display.set_caption("MyGame")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
